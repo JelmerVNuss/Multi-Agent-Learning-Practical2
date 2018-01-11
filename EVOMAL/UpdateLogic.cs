f2bfab95009eaa33ac9b03127232124d5cc47c88
SvdWegen <svdwegen@xs4all.nl>
2018-01-11 14:42:24 +0100

    Add scoretable code stub

--- EVOMAL/UpdateLogic.cs
+++ EVOMAL/UpdateLogic.cs
@@ -18,7 +18,51 @@ namespace EVOMAL
         /// strategies consists of 11 strategy objects
         static public double[,] scoreTable(int nrrounds, int nrrestarts, Strategy[] strategies, double noise)
         {
-            return new double[11, 11];
+            int numberOfStrategies = strategies.Length;
+            double[,] scoreTable = new double[numberOfStrategies, numberOfStrategies];
+
+
+            double totalPayoff = 0;
+            // TODO for all strategy combinations
+            for (int i = 0; i < nrrestarts; i++)
+            {
+                for (int j = 0; j < nrrounds; j++)
+                {
+                    ownAction = ;// strategy1 action + noise
+                    if randomnumber < noise, choose random
+                        else use strategy action
+                    opponentsAction = ;// strategy2 action + noise
+
+                    double payoff = getPayoff(ownAction, opponentsAction);
+
+                    totalPayoff += payoff;
+                }
+            }
+            // TODO add payoff to row/column scoretable, take average
+            scoreTable[ownStrategy, opponentsStrategy] = totalPayoff/(nrrestarts * nrrounds);
+
+            return scoreTable;
+        }
+
+
+
+        // TODO make double payoff type
+        static private int getPayoff(int myAction, int yourAction)
+        {
+            int payoff = 0; 
+            if(myAction ==0 && yourAction ==0)
+            {
+                payoff = 3;
+            }
+            else if (myAction == 1 && yourAction == 0)
+            {
+                payoff = 5;
+            }
+            else if (myAction == 1 && yourAction == 1)
+            {
+                payoff = 1;
+            }
+            return payoff;    
         }
 
         /// TODO STUDENT
